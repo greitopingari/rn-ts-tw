c9a6898242dc5b9e3eeabf8bfbb07c80ad4018f9
greitopingari <70757461+greitopingari@users.noreply.github.com>
2024-03-16 16:33:23 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}"], // Make sure you update this according to your folder structures
+  content: ["./App.{js,jsx,ts,tsx}"], // Make sure you update this according to your folder structure
   theme: {
     extend: {
       colors: {
