33dcbad04b96f2ec7cd742930bc0fe21f13233d3
greitopingari <greitopingari@gmail.com>
2024-03-16 16:31:35 +0100

    boilerplate init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}"], // Make sure you update this according to your folder structures
+  theme: {
+    extend: {
+      colors: {
+        primary: "#240A34"
+      }
+    },
+  },
+  plugins: [],
+};
